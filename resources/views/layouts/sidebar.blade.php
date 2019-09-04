73611b1247c4d818c4b631bbc5572787b299041a
ilhamwibisono13 <43263625+ilhamwibisono13@users.noreply.github.com>
2019-09-04 23:01:18 +0700

    rename fix

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,139 @@
+<!-- Page content -->
+<div class="page-content">
+
+    <!-- Main sidebar -->
+    <div class="sidebar sidebar-dark sidebar-main sidebar-expand-md">
+
+        <!-- Sidebar mobile toggler -->
+        <div class="sidebar-mobile-toggler text-center">
+            <a href="#" class="sidebar-mobile-main-toggle">
+                <i class="icon-arrow-left8"></i>
+            </a>
+            Navigation
+            <a href="#" class="sidebar-mobile-expand">
+                <i class="icon-screen-full"></i>
+                <i class="icon-screen-normal"></i>
+            </a>
+        </div>
+        <!-- /sidebar mobile toggler -->
+
+
+        <!-- Sidebar content -->
+        <div class="sidebar-content">
+
+            <!-- User menu -->
+            <div class="sidebar-user">
+                <div class="card-body">
+                    <div class="media">
+                        <div class="mr-3">
+                            <a href="#"><img src="../../../../global_assets/images/placeholders/placeholder.jpg"
+                                    width="38" height="38" class="rounded-circle" alt=""></a>
+                        </div>
+
+                        <div class="media-body">
+                            <div class="media-title font-weight-semibold">Victoria Baker</div>
+                            <div class="font-size-xs opacity-50">
+                                <i class="icon-pin font-size-sm"></i> &nbsp;Santa Ana, CA
+                            </div>
+                        </div>
+
+                        <div class="ml-3 align-self-center">
+                            <a href="#" class="text-white"><i class="icon-cog3"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /user menu -->
+
+
+            <!-- Main navigation -->
+            <div class="card card-sidebar-mobile">
+                <ul class="nav nav-sidebar" data-nav-type="accordion">
+
+                    <!-- Main -->
+                    <li class="nav-item-header">
+                        <div class="text-uppercase font-size-xs line-height-xs">Main</div> <i class="icon-menu"
+                            title="Main"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-link active">
+                            <i class="icon-home4"></i>
+                            <span>
+                                Halaman Utama
+                            </span>
+                        </a>
+                    </li> 
+                    <li class="nav-item">
+                        <a href="{{route('login')}}" class="nav-link">
+                            <i class="icon-users"></i>
+                            <span>
+                                Login
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('signin')}}" class="nav-link">
+                            <i class="icon-users"></i>
+                            <span>
+                                Tambah Akun
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('pendaftaran')}}" class="nav-link">
+                            <i class="icon-pencil5"></i>
+                            <span>
+                                Pendaftaran
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-link">
+                            <i class="icon-accessibility2"></i>
+                            <span>
+                                Rawat Jalan
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-link">
+                            <i class="icon-bed2"></i>
+                            <span>
+                                Rawat Inap
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-link">
+                            <i class="icon-coins"></i>
+                            <span>
+                                Keuangan
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-link">
+                            <i class="icon-plus-circle2"></i>
+                            <span>
+                                Obat
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-link">
+                            <i class="icon-snowflake"></i>
+                            <span>
+                                Penyakit
+                            </span>
+                        </a>
+                    </li>
+                    
+                </ul>
+            </div>
+            <!-- /main navigation -->
+
+        </div>
+        <!-- /sidebar content -->
+
+    </div>
+    <!-- /main sidebar -->
