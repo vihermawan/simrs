429998478fd5f1baf9f4b3c614828b072d4f67d7
Pratita Sujatmika <31695797+mikorail@users.noreply.github.com>
2019-09-21 18:53:28 +0700

    done inap

--- resources/views/rawatinap/ruang.blade.php
+++ resources/views/rawatinap/ruang.blade.php
@@ -24,7 +24,7 @@
             <div class="d-flex">
                 <div class="breadcrumb">
                     <a href="index.html" class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Home</a>
-                    <span class="breadcrumb-item active">Modal Ruangan</span>
+                    <span class="breadcrumb-item active">Ruangan</span>
                 </div>
 
                 <a href="#" class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a>
