0549ab84052b451a98880ba54484e64f202e01fa
Vicky Hermawan <hv3344588@gmail.com>
2019-09-22 15:21:40 +0700

    pendaftaran baru2

--- resources/views/pendaftaran/pendaftaran.blade.php
+++ resources/views/pendaftaran/pendaftaran.blade.php
@@ -85,8 +85,8 @@
 
                         <div class="card-body">
                             <div class="col-lg-8">
-                                <button type="button" class="btn bg-primary btn-labeled btn-labeled-left" data-toggle="modal" data-target="#modal_theme_success"><b><i class="icon-reading"></i></b> Tambah Pasien Baru</button>
-                                <button type="button" class="btn bg-primary btn-labeled btn-labeled-left" data-toggle="modal" data-target="#modal_theme_success"><b><i class="icon-reading"></i></b> Tambah Pendaftaran Baru</button>
+                                <button type="button" class="btn bg-success btn-labeled btn-labeled-left" data-toggle="modal" data-target="#modal_theme_success"><b><i class="icon-reading"></i></b> Tambah Pasien Baru</button>
+                                <button type="button" class="btn bg-primary btn-labeled btn-labeled-left" data-toggle="modal" data-target="#modal_theme_primary"><b><i class="icon-reading"></i></b> Tambah Pendaftaran Baru</button>
                             </div>
                             
                         </div>
@@ -173,22 +173,22 @@
 										</div>
 
                                         <div class="form-group row">
-                                                        <label class="col-lg-4 col-form-label">Jenis Kelamin:</label>
-                                                        <div class="col-lg-8">
-                                                            <div class="form-check form-check-inline">
-                                                                <label class="form-check-label">
-                                                                    <div class="uniform-choice"><span class="checked"><input type="radio" class="form-input-styled" name="gender" checked="" data-fouc=""></span></div>
-                                                                    Laki - laki
-                                                                </label>
-                                                            </div>
+                                            <label class="col-lg-4 col-form-label">Jenis Kelamin:</label>
+                                                <div class="col-lg-8">
+                                                    <div class="form-check form-check-inline">
+                                                        <label class="form-check-label">
+                                                            <div class="uniform-choice"><span class="checked"><input type="radio" class="form-input-styled" name="gender" checked="" data-fouc=""></span></div>
+                                                                Laki - laki
+                                                        </label>
+                                                    </div>
 
-                                                            <div class="form-check form-check-inline">
-                                                                <label class="form-check-label">
-                                                                    <div class="uniform-choice"><span><input type="radio" class="form-input-styled" name="gender" data-fouc=""></span></div>
-                                                                    Perempuan
-                                                                </label>
-                                                            </div>
-                                                        </div>
+                                                    <div class="form-check form-check-inline">
+                                                        <label class="form-check-label">
+                                                            <div class="uniform-choice"><span><input type="radio" class="form-input-styled" name="gender" data-fouc=""></span></div>
+                                                                Perempuan
+                                                        </label>
+                                                    </div>
+                                                </div>
                                          </div>
 
                                          <div class="form-group row">
@@ -345,11 +345,107 @@
                                 <button type="button" class="btn btn-primary">Simpan Data</button>
 							</div>
 						</form>
+					</div>  
+                </div>
+				<!-- /2 columns form -->
+
+        <!--End Modal Pendaftaran-->
+    </div>
+    <!--Modal Form Pendaftaran Baru -->
+    <div id="modal_theme_primary" class="modal fade" tabindex="-2">
+					<div class="modal-dialog modal-full">
+						<div class="modal-content">
+							<div class="modal-header bg-primary">
+								<h6 class="modal-title">Form Pendaftaran Baru</h6>
+								<button type="button" class="close" data-dismiss="modal">&times;</button>
+							</div>
+
+							<div class="modal-body">
+                            <div class="row">
+					           <!-- 2 columns form -->
+				
+					<div class="card-header header-elements-inline">
+	
 					</div>
-			
+
+					<div class="card-body">
+						<form action="#">
+							<div class="row">
+								<div class="col-md-4">
+									<fieldset>
+										<legend class="text-uppercase font-size-sm font-weight-bold"><i class="icon-reading mr-2"></i> IDENTITAS PASIEN</legend>
+
+										<div class="form-group row">
+											<label class="col-lg-4 col-form-label">Nama Pasien :</label>
+											<div class="col-lg-8">
+												<input type="text" class="form-control">
+											</div>
+										</div>
+
+                                        <div class="form-group row">
+                                                        <label class="col-lg-4 col-form-label">Jenis Kelamin:</label>
+                                                        <div class="col-lg-8">
+                                                            <div class="form-check form-check-inline">
+                                                                <label class="form-check-label">
+                                                                    <div class="uniform-choice"><span class="checked"><input type="radio" class="form-input-styled" name="gender" checked="" data-fouc=""></span></div>
+                                                                    Laki - laki
+                                                                </label>
+                                                            </div>
+
+                                                            <div class="form-check form-check-inline">
+                                                                <label class="form-check-label">
+                                                                    <div class="uniform-choice"><span><input type="radio" class="form-input-styled" name="gender" data-fouc=""></span></div>
+                                                                    Perempuan
+                                                                </label>
+                                                            </div>
+                                                        </div>
+                                         </div>
+                                         <div class="form-group row">
+											<label class="col-lg-4 col-form-label">Nama Petugas:</label>
+											<div class="col-lg-8">
+														<input type="text" class="form-control">
+											</div>
+										</div>
+                                   
+                                        <div class="form-group row">
+										    <label class="col-lg-4 col-form-label">Asuransi:</label>
+										        <div class="col-lg-8">
+											        <select class="form-control form-control-select">
+                                                        <option>BPJS</option>
+                                                        <option>Non BPJS</option>
+											        </select>
+										        </div>
+									    </div>
+
+                                        <div class="form-group row">
+										    <label class="col-lg-4 col-form-label">Nama Poli:</label>
+										        <div class="col-lg-8">
+											        <select class="form-control form-control-select">
+                                                        <option>Poli Anak</option>
+                                                        <option>Poli Bedah</option>
+                                                        <option>Poli Gigi</option>
+                                                        <option>Poli THT</option>
+                                                        <option>Poli Penyakit Dalam</option>
+											        </select>
+										        </div>
+									    </div>
+
+                                       
+									</fieldset>
+								</div>
+                               
+							</div>
+
+							<div class="text-right">
+                                <button type="button" class="btn btn-primary">Simpan Data</button>
+							</div>
+						</form>
+					</div>
+            </div>
 				<!-- /2 columns form -->
 
         <!--End Modal Pendaftaran-->
 
-    </div>
 </div>
+
+ 
